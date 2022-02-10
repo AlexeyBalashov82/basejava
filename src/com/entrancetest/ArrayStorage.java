da93386d8648ce9e5ab7e27ec44f4e357d8e2c58
AlexB82 <82.abalashov@gmail.com>
2022-02-10 15:14:10 +0300

    Вступительно ДЗ на BaseJave готово к первой проверке

--- src/com/entrancetest/ArrayStorage.java
+++ src/com/entrancetest/ArrayStorage.java
@@ -1,32 +1,76 @@
 package com.entrancetest;
 
+import java.util.Arrays;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int resumeCount = 0;
 
     void clear() {
+        for (int i = 0; i < resumeCount; i++) {
+            storage[i] = null;
+        }
+        resumeCount = 0;
     }
 
     void save(Resume r) {
+        if (searchIndex(r.toString()) < 0) {
+            storage[resumeCount] = r;
+            resumeCount++;
+        } else {
+            System.out.println("Not unique uuid: " + r);
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        int i = searchIndex(uuid);
+        if (i < 0) {
+            System.out.println("Not found resume with uuid: " + uuid);
+        }
+        return (i < 0) ? null : storage[i];
     }
 
     void delete(String uuid) {
+        int i = searchIndex(uuid);
+        if (i < 0) {
+            System.out.println("Not found resume with uuid: " + uuid);
+        } else {
+            storageSetRange(Arrays.copyOfRange(storage, 0, i), 0);
+            storageSetRange(Arrays.copyOfRange(storage, i + 1, resumeCount), i);
+            resumeCount--;
+            storage[resumeCount] = null;
+        }
     }
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
+    private int searchIndex(String uuid) {
+        boolean isResumeInStorage = false;
+        int i = 0;
+        while (i < resumeCount) {
+            isResumeInStorage = storage[i].toString().equals(uuid);
+            if (isResumeInStorage) {
+                break;
+            }
+            i++;
+        }
+        return (isResumeInStorage) ? i : -1;
+    }
+
+    private void storageSetRange(Resume[] sourceStorage, int startIndex) {
+        for (int i = 0; i <= sourceStorage.length - 1; i++) {
+            storage[startIndex + i] = sourceStorage[i];
+        }
+    }
+
     Resume[] getAll() {
-        return new Resume[0];
+        return (resumeCount > 0) ? Arrays.copyOfRange(storage, 0, resumeCount) : new Resume[0];
     }
 
     int size() {
-        return 0;
+        return resumeCount;
     }
 }
--- src/com/entrancetest/MainTestArrayStorage.java
+++ src/com/entrancetest/MainTestArrayStorage.java
@@ -24,16 +24,27 @@ public class MainTestArrayStorage {
         System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
 
         printAll();
-        ARRAY_STORAGE.delete(r1.uuid);
+        ARRAY_STORAGE.delete(r2.uuid);
+        System.out.println("Size: " + ARRAY_STORAGE.size());
         printAll();
         ARRAY_STORAGE.clear();
         printAll();
 
         System.out.println("Size: " + ARRAY_STORAGE.size());
+
+        ARRAY_STORAGE.save(r1);
+        ARRAY_STORAGE.save(r2);
+        ARRAY_STORAGE.save(r3);
+
+        System.out.println("Get r1: " + ARRAY_STORAGE.get(r3.uuid));
+        System.out.println("Size: " + ARRAY_STORAGE.size());
+
+
     }
 
     static void printAll() {
         System.out.println("\nGet All");
+
         for (Resume r : ARRAY_STORAGE.getAll()) {
             System.out.println(r);
         }
